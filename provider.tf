bb9b4321a1e04a279120808d9a5ba9c51a466c73
aamair92ameen <118685304+aamair92ameen@users.noreply.github.com>
2023-03-15 15:16:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   region  = "eu-west-1" # Don't change the region
+}
   S3 Bucket: "3.devops.candidate.exam"
 Region: "eu-west-1"
 Key: "Mohammed.Aamair"
-}
-
 # Add your S3 backend configuration here
